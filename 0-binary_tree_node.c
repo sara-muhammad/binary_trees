2393f331e34a6c7e35dab6df85a6ab3dac33f1d7
sara-muhammad <saraelakaad@gmail.com>
2024-03-01 02:41:55 +0200

    upload

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,8 +9,7 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *born;
-    born = malloc(sizeof(binary_tree_t));
+	binary_tree_t *born = malloc(sizeof(binary_tree_t));
 
 	if (born == NULL)
 	{
