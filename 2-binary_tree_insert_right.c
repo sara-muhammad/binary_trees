795beeca912112f205c081a573844efbc47abbc6
sara-muhammad <saraelakaad@gmail.com>
2024-03-01 02:23:32 +0200

    upload

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - insert node at left
+ * binary_tree_insert_right - insert node at left
  * @parent: the parent node
  * @value: the value stored in the new node
  * Return: pointer to the created node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
@@ -18,11 +18,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
-		new->left = parent->left;
-		parent->left->parent = new;
+		new->right = parent->right;
+		parent->right->parent = new;
 	}
-	parent->left = new;
+	parent->right = new;
 	return (new);
 }
